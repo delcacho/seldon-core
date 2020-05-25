2080f354dc46d813ac15756da84edb5bb29a17ab
delcacho <delcacho@gmail.com>
2020-05-25 12:02:11 +0200

    Extend timeouts

--- operator/controllers/seldondeployment_controller.go
+++ operator/controllers/seldondeployment_controller.go
@@ -724,10 +724,10 @@ func createContainerService(deploy *appsv1.Deployment,
 	}
 
 	if con.LivenessProbe == nil {
-		con.LivenessProbe = &corev1.Probe{Handler: corev1.Handler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString(portType)}}, InitialDelaySeconds: 60, PeriodSeconds: 5, SuccessThreshold: 1, FailureThreshold: 3, TimeoutSeconds: 1}
+		con.LivenessProbe = &corev1.Probe{Handler: corev1.Handler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString(portType)}}, InitialDelaySeconds: 180, PeriodSeconds: 60, SuccessThreshold: 1, FailureThreshold: 3, TimeoutSeconds: 60}
 	}
 	if con.ReadinessProbe == nil {
-		con.ReadinessProbe = &corev1.Probe{Handler: corev1.Handler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString(portType)}}, InitialDelaySeconds: 120, PeriodSeconds: 5, SuccessThreshold: 1, FailureThreshold: 3, TimeoutSeconds: 1}
+		con.ReadinessProbe = &corev1.Probe{Handler: corev1.Handler{TCPSocket: &corev1.TCPSocketAction{Port: intstr.FromString(portType)}}, InitialDelaySeconds: 180, PeriodSeconds: 60, SuccessThreshold: 1, FailureThreshold: 3, TimeoutSeconds: 60}
 	}
 
 	// Add livecycle probe
